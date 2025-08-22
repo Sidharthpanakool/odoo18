8931dfea1fd512b1a6d5f91f54a9113e1335a4f0
Sidharth@0312 <sidharthpanakool@gmail.com>
2025-08-22 17:09:59 +0530

    latest repair order can see in snippet

--- vehicle_repair_management/static/src/js/snippet.js
+++ vehicle_repair_management/static/src/js/snippet.js
@@ -1,3 +1,38 @@
+///** @odoo-module */
+////import { renderToElement } from "@web/core/utils/render";
+////import publicWidget from "@web/legacy/js/public/public_widget";
+////import { rpc } from "@web/core/network/rpc";
+//console.log("Snippet")
+//odoo.define('vehicle_repair_management.snippet', function(require) {
+//'use strict';
+//var PublicWidget = require('web.public.widget');
+//var rpc = require('web.rpc');
+//var core = require('web.core');
+//var qweb = core.qweb;
+//var Dynamic = PublicWidget.Widget.extend({
+//    selector : '.categories_section',
+//    willStart:async function() {
+//    var self=this;
+//    await rpc.query({
+//    route:'/get_top_vehicles',}).then((data)=>
+//    {
+//    this.data=data;
+//    });
+//    },
+//    start:function(){
+//    var chunks=_.chunk(this.data,4)
+//    chunks[0].is_active=true
+//    this.$el.find('#top_products_carousel').html(
+//    qweb.render('vehicle_repair_management.category_data',{chunks
+//    })
+//    )
+//    },
+//    });
+//    PublicWidget.registry.categories_section = Dynamic;``
+//    return Dynamic;
+//    });
+
+
 /** @odoo-module */
 import { renderToElement } from "@web/core/utils/render";
 import publicWidget from "@web/legacy/js/public/public_widget";
@@ -13,4 +48,3 @@ publicWidget.registry.get_product_tab = publicWidget.Widget.extend({
         }
     },
 });
-
