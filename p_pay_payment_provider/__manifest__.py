a3d60b2149d236488510014ad161085abdcd4122
Sidharth@0312 <sidharthpanakool@gmail.com>
2025-09-08 17:16:30 +0530

    Payment Provider

--- p_pay_payment_provider/__manifest__.py
+++ p_pay_payment_provider/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Payment Provider',
+    'name': 'P PAY',
     'version': '18.0.1.0.0',
     'license': "LGPL-3",
     'category': 'Sales/pos',
@@ -7,8 +7,8 @@
     'summary': 'Payment Provider',
     'description': "Payment Provider by SIDHARTH P",
     'author': "ASMI",
-    'data': [
-
+    'data': ['data/payment_method.xml',
+            'data/payment_provider_record.xml',
              ],
     'assets': {
         'point_of_sale._assets_pos': [
---
+++ p_pay_payment_provider/data/payment_method.py
@@ -0,0 +1,9 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+class PaymentMethod(models.Model):
+    _inherit = 'payment.method'
+
+
+
