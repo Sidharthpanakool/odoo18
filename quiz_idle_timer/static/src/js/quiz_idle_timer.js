f5e739f4e1ee6c348edf8d86ec5f40fe0d3d863c
Sidharth@0312 <sidharthpanakool@gmail.com>
2025-09-24 16:48:49 +0530

    sept 24 2025

--- quiz_idle_timer/static/src/js/quiz_idle_timer.js
+++ quiz_idle_timer/static/src/js/quiz_idle_timer.js
@@ -1,9 +1,137 @@
-///** @odoo-module **/
-//
+///* @odoo-module */
+//import { Component, useState } from "@odoo/owl";
+//import { rpc } from "@web/core/network/rpc";
+//import { registry } from "@web/core/registry";
+//import { session } from "@web/session";
+//import { useService } from "@web/core/utils/hooks";
+//import { _t } from "@web/core/l10n/translation";
+//const { onMounted, mount } = owl
+//class TimerSystrayItem extends Component{
+//    static template="auto_logout_idle_user_odoo.TimerSystray"
+//    setup(){
+//        super.setup();
+//        this.get_idle_time();
+//        this.state = useState({
+//           idle_time: null,
+//        })
+//    }
+//    get_idle_time() {
+//        var self = this
+//        var now = new Date().getTime();
+//        rpc('/get_idle_time/timer', {
+//        }).then((data) => {
+//            if (data) {
+//                self.minutes = data
+//                self.idle_timer()
+//            }
+//         });
+//    }
+//    /**
+//    passing values of the countdown to the xml
+//    */
+//    idle_timer() {
+//        var self = this
+//        var nowt = new Date().getTime();
+//        var date = new Date(nowt);
+//        date.setMinutes(date.getMinutes() + self.minutes);
+//        var updatedTimestamp = date.getTime();
+//        /** Running the count down using setInterval function */
+//        var idle = setInterval(function() {
+//            var now = new Date().getTime();
+//            var distance = updatedTimestamp - now;
+//            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//            if (hours && days) {
+//                self.state.idle_time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+//            } else if (hours) {
+//                self.state.idle_time = hours + "h " + minutes + "m " + seconds + "s ";
+//            } else {
+//                self.state.idle_time = minutes + "m " + seconds + "s ";
+//            }
+//            /** if the countdown is zero the link is redirect to the login page*/
+//            if (distance < 0) {
+//                clearInterval(idle);
+//                self.state.idle_time = "EXPIRED";
+//                location.replace("/web/session/logout")
+//            }
+//        }, 1000);
+//        /**
+//        checking if the onmouse-move event is occur
+//        */
+//        document.onmousemove = () => {
+//            var nowt = new Date().getTime();
+//            var date = new Date(nowt);
+//            date.setMinutes(date.getMinutes() + self.minutes);
+//            updatedTimestamp = date.getTime();
+//        };
+//         /**
+//        checking if the onkeypress event is occur
+//        */
+//        document.onkeypress = () => {
+//            var nowt = new Date().getTime();
+//            var date = new Date(nowt);
+//            date.setMinutes(date.getMinutes() + self.minutes);
+//            updatedTimestamp = date.getTime();
+//        };
+//        /**
+//        checking if the onclick event is occur
+//        */
+//        document.onclick = () => {
+//            var nowt = new Date().getTime();
+//            var date = new Date(nowt);
+//            date.setMinutes(date.getMinutes() + self.minutes);
+//            updatedTimestamp = date.getTime();
+//        };
+//        /**
+//        checking if the ontouchstart event is occur
+//        */
+//        document.ontouchstart = () => {
+//            var nowt = new Date().getTime();
+//            var date = new Date(nowt);
+//            date.setMinutes(date.getMinutes() + self.minutes);
+//            updatedTimestamp = date.getTime();
+//        }
+//        /**
+//        checking if the onmousedown event is occur
+//        */
+//        document.onmousedown = () => {
+//            var nowt = new Date().getTime();
+//            var date = new Date(nowt);
+//            date.setMinutes(date.getMinutes() + self.minutes);
+//            updatedTimestamp = date.getTime();
+//        }
+//        /**
+//        checking if the onload event is occur
+//        */
+//        document.onload = () => {
+//            var nowt = new Date().getTime();
+//            var date = new Date(nowt);
+//            date.setMinutes(date.getMinutes() + self.minutes);
+//            updatedTimestamp = date.getTime();
+//        }
+//    }
+//}
+//export const systrayItem = {
+//    Component: TimerSystrayItem
+//};
+//registry.category("systray").add("auto_logout_idle_user_odoo.TimerSystray",systrayItem, {sequence:25});
+
+
+
+
+
+
+
+/** @odoo-module **/
+
 //import { deserializeDateTime } from "@web/core/l10n/dates";
 //import publicWidget from "@web/legacy/js/public/public_widget";
 //const { DateTime } = luxon;
-//
+//import { useIdleTimer } from "./utils/use_idle_timer";
+
+
 //publicWidget.registry.SurveyTimerWidget = publicWidget.Widget.extend({
 //    //--------------------------------------------------------------------------
 //    // Widget
@@ -83,3 +211,44 @@
 //});
 //
 //export default publicWidget.registry.SurveyTimerWidget;
+
+
+
+// Timeout in seconds
+var timeout = 10; // 10 seconds
+
+// You don't have to change anything below this line, except maybe
+// the alert('Welcome back!') :-)
+// ----------------------------------------------------------------
+var pos = '', prevpos = '', timer = 0, interval = timeout / 5 * 1000;
+timeout = timeout * 1000 - interval;
+
+function mouseHasMoved(e){
+
+    console.log("mouseHasMoved")
+
+    document.onmousemove = null;
+    prevpos = pos;
+    pos = e.pageX + '+' + e.pageY;
+    if(timer > timeout){
+        timer = 0;
+//        alert('Welcome back!');
+
+    }
+}
+
+setInterval(function(){
+    console.log("idle")
+
+
+    if(pos == prevpos){
+//        alert('Time is running!');
+        timer += interval;
+    }else{
+        timer = 0;
+        prevpos = pos;
+    }
+    document.onmousemove = function(e){
+        mouseHasMoved(e);
+    }
+}, interval);
