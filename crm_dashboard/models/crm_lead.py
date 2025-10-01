db410e9bb61f9c0b82fdfcf82b0712837fd87e9e
Sidharth@0312 <sidharthpanakool@gmail.com>
2025-10-01 16:33:24 +0530

    oct 1 2025

--- crm_dashboard/models/crm_lead.py
+++ crm_dashboard/models/crm_lead.py
@@ -8,16 +8,15 @@ class CrmLead(models.Model):
        company_id = self.env.company
        leads = self.search([('company_id', '=', company_id.id),
                             ('user_id', '=', self.env.user.id)])
+       my_leads = leads.filtered(lambda r: r.type == 'lead')
+       print('my_leads', my_leads)
 
        invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice'),('state', '=', 'posted')])
 
        total_amount_sum = sum(invoice.amount_total for invoice in invoices)
 
+       print('invoices', invoices)
        print("total",total_amount_sum)
-       print('invoices',invoices)
-
-       my_leads = leads.filtered(lambda r: r.type == 'lead')
-       print('my_leads',my_leads)
 
        my_opportunity = leads.filtered(lambda r: r.type == 'opportunity')
 
@@ -28,10 +27,20 @@ class CrmLead(models.Model):
        print('currency',currency)
        print('exp_revenue',expected_revenue)
 
+       won =self.search([('company_id', '=', company_id.id),('user_id', '=', self.env.user.id),('stage_id', '=', 4),('active','=',True),('probability','=',100)])
+       # .with_context(active_test=False)
+       loss=self.search([('company_id', '=', company_id.id),('user_id', '=', self.env.user.id),('stage_id', '!=',4),('active','=',False),('probability','=',  0)])
+       print(won)
+       print('won',len(won))
+       print(loss)
+       print('loss',len(loss))
        return {
            'total_leads': len(my_leads),
            'total_opportunity': len(my_opportunity),
            'expected_revenue': expected_revenue,
            'currency': currency,
            'invoice_amt_sum' : total_amount_sum,
+           'won': len(won),
+           'loss': len(loss),
+
        }
--- crm_dashboard/static/src/js/dashboard.js
+++ crm_dashboard/static/src/js/dashboard.js
@@ -18,7 +18,104 @@ class CrmDashboard extends Component {
      document.getElementById('my_opportunity').innerHTML = `<span>${result.total_opportunity}</span>`;
      document.getElementById('exp_revenue').innerHTML = `<span>${result.currency}${result.expected_revenue}</span>`;
      document.getElementById('revenue').innerHTML = `<span>${result.currency}${result.invoice_amt_sum}</span>`;
+     document.getElementById('won').innerHTML = `<span>${result.won}</span>`;
+     document.getElementById('loss').innerHTML = `<span>${result.loss}</span>`;
+
+
+
   }
+  redirect_to_leads(group, filter="all") {
+               console.log("aaa")
+
+//
+////        this.dropdown.close();
+////        const context = {
+////            // Necessary because activity_ids of mail.activity.mixin has auto_join
+////            // So, duplicates are faking the count and "Load more" doesn't show up
+////            force_search_count: 1,
+////        };
+//
+//        if (group.model === "account.move") {
+//            this.action.doAction("account.view_invoice_form_l10n_sg", {
+//                additionalContext: {
+////                    active_ids: group.activity_ids,
+//                    active_model: "account.move",
+//                },
+//            });
+//            return;
+//        }
+////        this.action.doAction(
+////            {
+//////                context,
+//////                domain,
+////                name: group.name,
+////                res_model: account.move,
+////                search_view_id: [false],
+////                type: "ir.actions.act_window",
+////                views,
+////            },
+////            {
+////                clearBreadcrumbs: true,
+////                viewType: group.view_type,
+////            }
+////        );
+//            this.action.doAction({
+//            type: 'ir.actions.act_window',
+//            name: _t('crm.lead.form'),
+//            target: 'current',
+//            res_id: activity.res_id,
+//            res_model: 'crm.lead',
+//            views: [[false, 'form']],
+//            });
+
+////        if (filter === "all") {
+////            context["search_default_activities_overdue"] = 1;
+////            context["search_default_activities_today"] = 1;
+////        }
+////        else if (filter === "overdue") {
+////            context["search_default_activities_overdue"] = 1;
+////        }
+////        else if (filter === "today") {
+////            context["search_default_activities_today"] = 1;
+////        }
+////        else if (filter === "upcoming_all") {
+////            context["search_default_activities_upcoming_all"] = 1;
+////        }
+//
+////        let domain = [["activity_user_id", "=", this.userId]];
+////        if (group.domain) {
+////            domain = Domain.and([domain, group.domain]).toList();
+////        }
+////        const views = this.availableViews(group);
+////    }
+    }
+
+//  redirect_to_leads(group, filter = "all") {
+//        // fetch the data from the button otherwise fetch the ones from the parent (.o_ActivityMenuView_activityGroup).
+//        const context = {};
+//        if (group.model === "crm.lead") {
+//            this.dropdown.close();
+//            if (filter === "my") {
+//                context["search_default_activities_overdue"] = 1;
+//                context["search_default_activities_today"] = 1;
+//            } else {
+//                context["search_default_activities_" + filter] = 1;
+//            }
+//            // Necessary because activity_ids of mail.activity.mixin has auto_join
+//            // So, duplicates are faking the count and "Load more" doesn't show up
+//            context["force_search_count"] = 1;
+//            context["active_test"] = 0; // to show lost leads in the activity
+//            this.action.doAction("crm.crm_lead_action_my_activities", {
+//                additionalContext: context,
+//                clearBreadcrumbs: true,
+//            });
+//        } else {
+//            return super.openActivityGroup(group, filter);
+//        }
+//    }
+
+
+
 
 }
 CrmDashboard.template = "crm_dashboard.CrmDashboard";
