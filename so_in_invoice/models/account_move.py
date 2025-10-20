dc78118d7559e0c86fafa08885c62b4b27ec3b85
Sidharth@0312 <sidharthpanakool@gmail.com>
2025-10-20 17:18:30 +0530

    oct 20 2025

--- so_in_invoice/models/account_move.py
+++ so_in_invoice/models/account_move.py
@@ -5,27 +5,35 @@ from odoo import api, models, fields, Command
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+
     related_sale_orders_ids = fields.Many2many(comodel_name='sale.order',
-                                               string ="Related SaleOrders" )
+                                               string ="Related SaleOrders",
+                                               domain="[('state', '=', 'sale'),('invoice_status','!=','invoiced'),('partner_id','=',partner_id)]"
+                                               )
 
-    @api.onchange('related_sale_orders_ids','partner_id')
+    @api.onchange('related_sale_orders_ids')
     def _order_line(self):
-        if self.related_sale_orders_ids:
+        order_line_list = []
+        for rec in self.related_sale_orders_ids.mapped('order_line'):
 
-            sale_ids = self.partner_id.mapped('sale_order_ids').filtered(lambda o: o.state == 'sale'
-                                                               and not o.invoice_status=='invoiced' and o.partner_id == self.partner_id)
-            if self.related_sale_orders_ids.id != sale_ids.id:
-                self.related_sale_orders_ids = sale_ids
+            order_line_list.append(Command.create({
+                'product_id': rec.product_id.id,
+                'quantity': rec.product_uom_qty,
+                'price_unit': rec.price_unit,
+                'price_subtotal': rec.price_subtotal,
+                'sale_line_ids': [(6, 0, [rec.id])],
+            }))
+            print(rec.id)
+        self.invoice_line_ids =  order_line_list
+        print(self.invoice_line_ids)
 
 
-            order_line_list = []
-            for rec in self.related_sale_orders_ids.mapped('order_line'):
-                order_line_list.append(Command.create({
-                    'product_id': rec.product_id,
-                    'quantity': rec.product_uom_qty,
-                    'sale_line_ids': [(6, 0, [rec.id])],
-                }))
-            self.invoice_line_ids =  order_line_list
+
+
+
+            # sale_ids = self.partner_id.mapped('sale_order_ids').filtered(lambda o: o.state == 'sale'
+            #                                                    and not o.invoice_status=='invoiced' and o.partner_id == self.partner_id)
+            # print(sale_ids)
 
 
 
